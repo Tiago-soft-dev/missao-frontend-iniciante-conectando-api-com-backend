ecc0b2d9d111f3df20966a15542d012c0fed836b
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-12 15:39:56 -0300

    Lista de Devmons carregada com Fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,52 @@ import Header from './components/Header/Header'
 function App() {
 
  
-  const java = {
-    nome: 'Java',
-    imagem: 'https://salvatore.academy/devmon/1_java.png',
-    evoluiPara: 'Kotlin'
-  }
-  const kotlin = {
-    nome: 'Kotlin',
-    imagem: 'https://salvatore.academy/devmon/2_kotlin.png',
-    evoluiPara: 'Android'
-  }
-  const android = {
-    nome: 'Android',
-    imagem: 'https://salvatore.academy/devmon/3_android.png',
-    evoluiPara: 'C'
-  }
-  const c = {
-    nome: 'C',
-    imagem: 'https://salvatore.academy/devmon/4_c.png',
-    evoluiPara: null
-  }
+  // const java = {
+  //   nome: 'Java',
+  //   imagem: 'https://salvatore.academy/devmon/1_java.png',
+  //   evoluiPara: 'Kotlin'
+  // }
+  // const kotlin = {
+  //   nome: 'Kotlin',
+  //   imagem: 'https://salvatore.academy/devmon/2_kotlin.png',
+  //   evoluiPara: 'Android'
+  // }
+  // const android = {
+  //   nome: 'Android',
+  //   imagem: 'https://salvatore.academy/devmon/3_android.png',
+  //   evoluiPara: 'C'
+  // }
+  // const c = {
+  //   nome: 'C',
+  //   imagem: 'https://salvatore.academy/devmon/4_c.png',
+  //   evoluiPara: null
+  // }
 
-  const cPlusPlus = {
-    nome: 'C++',
-    imagem: 'https://salvatore.academy/devmon/5_cpp.png',
-    evoluiPara: 'C#'
+  // const cPlusPlus = {
+  //   nome: 'C++',
+  //   imagem: 'https://salvatore.academy/devmon/5_cpp.png',
+  //   evoluiPara: 'C#'
+  // }
+  // const devmons = [java,kotlin,android,c, cPlusPlus]
+
+  const devmons = []
+
+  async function fetchData() {
+    
+    const apiUrl = 'https://backend-integrar-com-o-frontend.onrender.com/personagem'
+
+    const response = await fetch(apiUrl)
+
+    const data = await response.json()
+    
+    console.log(45, data);
+    
   }
-  const devmons = [java,kotlin,android,c, cPlusPlus]
- 
+  fetchData()
+
+
+  
+
   return (
     <>
       Lista de Cards com Map:
@@ -52,7 +70,7 @@ function App() {
         })}
       </div>
 
-      <br />
+      {/* <br />
       <br />
       Lista de Cards 2:
       <br />
@@ -62,7 +80,7 @@ function App() {
         <Card nome="Java" imagem="https://salvatore.academy/devmon/1_java.png" />
         <Card nome="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png" />
         <Card nome="Android" imagem="https://salvatore.academy/devmon/3_android.png" />
-      </div>
+      </div> */}
 
     </>
   )
